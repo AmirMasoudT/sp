1e6527ba24ea8ef432588865bf98259fd1e916e1
ProjectX <projectx9302@gmail.com>
2024-09-25 08:15:30 +0330

    Initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ border: "1px solid black", height: "100vh" }}>
+      Hello from the other side
     </div>
   );
 }
-
 export default App;
