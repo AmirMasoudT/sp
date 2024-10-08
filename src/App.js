2268e7aaf782ac398f1c2fed4c50a056e3e141b0
ProjectX <projectx9302@gmail.com>
2024-10-08 15:04:34 +0330

    LpList Progress Stage 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Box, Stack } from "@mui/system";
 import Header from "./components/Header";
 import LpList from "./components/LpList";
+import { Grid2 } from "@mui/material";
 
 function App() {
   return (
-    <Box>
+    <Box sx={{ height: "100vh", overflow: "auto" }}>
       <Header />
-      <Stack>
-        <LpList />
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Grid2 container sx={{ height: "70vh" }} size={{ xs: 12, md: 5 }}>
+          <Grid2 item sx={{ height: "60%", overflowY: "auto" }}>
+            <LpList />
+          </Grid2>
+        </Grid2>
       </Stack>
     </Box>
   );
--- src/components/LpList.js
+++ src/components/LpList.js
@@ -1,7 +1,59 @@
+import { Box, Stack } from "@mui/system";
 import { fakeData } from "../data/fakeData";
+import { Divider, Typography } from "@mui/material";
 
 const LpList = () => {
-  return <div>{fakeData[0].licenseNo}</div>;
+  return (
+    <Stack divider={<Divider flexItem />}>
+      <Stack
+        direction={"row"}
+        divider={<Divider orientation="vertical" flexItem />}
+        sx={{ bgcolor: "secondary.main", color: "primary.main" }}
+      >
+        <Typography width={120} textAlign={"center"} alignContent={"center"}>
+          شماره پلاک
+        </Typography>
+        <Typography width={80} textAlign={"center"} alignContent={"center"}>
+          زمان تردد
+        </Typography>
+        <Typography width={150} textAlign={"center"} alignContent={"center"}>
+          علت تعقیب
+        </Typography>
+        <Typography width={350} textAlign={"center"} alignContent={"center"}>
+          محل عبور
+        </Typography>
+      </Stack>
+      {fakeData.map((car) => {
+        return (
+          <Stack
+            direction={"row"}
+            divider={<Divider orientation="vertical" flexItem />}
+          >
+            <Typography
+              width={120}
+              textAlign={"center"}
+              alignContent={"center"}
+            >
+              {car.licenseNo}
+            </Typography>
+            <Typography width={80} textAlign={"center"} alignContent={"center"}>
+              {car.travelTime}
+            </Typography>
+            <Typography
+              width={150}
+              textAlign={"center"}
+              alignContent={"center"}
+            >
+              {car.pursuitReason}
+            </Typography>
+            <Typography width={350} textAlign={"right"} alignContent={"center"}>
+              {car.passageWay}
+            </Typography>
+          </Stack>
+        );
+      })}
+    </Stack>
+  );
 };
 
 export default LpList;
